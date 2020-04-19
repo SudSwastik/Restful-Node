db157bb60fc30a33093804c8cc1856bb0c417e89
SudarshanKumar <sudarshan4309@gmail.com>
2020-04-20 02:28:33 +0530

    Added Lint to Project and Created Book Router

---
+++ README.md
@@ -0,0 +1,13 @@
+# Use Linting 
+
+> Install Eslint for Project
+```
+$ npm i eslint -D
+
+```
+
+>To run Lint
+- Add "lint": "eslint ." in package.json script 
+```
+$ npm run lint
+```
--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-var express = require('express');
+const express = require('express');
+const app = express();
+const bookRouter = express.Router();
+const port = process.env.PORT || 3000;
 
-var app = express();
+bookRouter.route('/books')
+  .get((req, res) => {
+    const response = { Hello: 'World' };
+    res.json(response);
+  });
+  
+app.use('/api', bookRouter);
 
-var port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-    res.send("Welcome To Node");
+app.get('/', (req, res) => {
+  res.send('Welcome To Node');
 });
 
 app.listen(port, () => {
-    console.log("Running on Port: "+ port);
-})
-
+  console.log(`Running on Port: ${port}`);
+});
