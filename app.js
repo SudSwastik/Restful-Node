ead29ff68d08dfdaabd78baafcf079bd2288195f
SudarshanKumar <sudarshan4309@gmail.com>
2020-04-20 12:08:51 +0530

    Seperated  Routing Logic In Routes

--- app.js
+++ app.js
@@ -3,45 +3,13 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI', {useNewUrlParser: true} );
-const bookRouter = express.Router();
 const Book = require('./model/bookModel');
 const port = process.env.PORT || 3000;
+const bookRouter = require('./routes/bookRouter')(Book);
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-bookRouter.route('/books')
-  .post((req, res) => {
-    const book = new Book(req.body);
-    book.save();
-    return res.status(201).json(book);
-  })
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-
-    Book.find(query, (err, books) => {
-      if(err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    }
-
-    );
-  });
-
-bookRouter.route('/books/:bookId')
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, book) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(book);
-    });
-  });
-
 app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
